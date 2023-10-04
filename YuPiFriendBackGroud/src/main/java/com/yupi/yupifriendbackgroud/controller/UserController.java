74e2f2e52925d29eef6cbd78328744f3303c7c59
Nameinvalid <2812967137@qq.com>
2023-10-04 21:38:29 +0800

    将接口返回值进行统一处理

--- YuPiFriendBackGroud/src/main/java/com/yupi/yupifriendbackgroud/controller/UserController.java
+++ YuPiFriendBackGroud/src/main/java/com/yupi/yupifriendbackgroud/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.yupi.yupifriendbackgroud.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.yupi.yupifriendbackgroud.common.BaseResponse;
+import com.yupi.yupifriendbackgroud.common.ErrorCode;
+import com.yupi.yupifriendbackgroud.common.ResultUtils;
 import com.yupi.yupifriendbackgroud.constant.UserConstant;
+import com.yupi.yupifriendbackgroud.exception.BusinessException;
 import com.yupi.yupifriendbackgroud.model.domain.User;
 import com.yupi.yupifriendbackgroud.model.domain.request.UserLoginRequest;
 import com.yupi.yupifriendbackgroud.model.domain.request.UserRegisterRequest;
@@ -30,9 +34,9 @@ public class UserController {
      * @return 新用户id
      */
     @PostMapping("/register")
-    public Long userRegister(@RequestBody UserRegisterRequest userRegisterRequest){
+    public BaseResponse<Long> userRegister(@RequestBody UserRegisterRequest userRegisterRequest){
         if (userRegisterRequest==null){
-            return null;
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
         }
         String userAccount = userRegisterRequest.getUserAccount();
         String userPassword = userRegisterRequest.getUserPassword();
@@ -40,7 +44,8 @@ public class UserController {
         if (StringUtils.isAnyBlank(userAccount,userPassword,checkPassword)){
             return null;
         }
-        return userService.userRegister(userAccount,userPassword,checkPassword);
+        Long result=userService.userRegister(userAccount,userPassword,checkPassword);
+        return ResultUtils.success(result);
     }
 
     /**
@@ -50,17 +55,20 @@ public class UserController {
      * @return 返回用户托名之后的信息
      */
     @PostMapping("/login")
-    public User userLogin(@RequestBody UserLoginRequest userLoginRequest, HttpServletRequest request){
+    public BaseResponse<User> userLogin(@RequestBody UserLoginRequest userLoginRequest, HttpServletRequest request){
         if (userLoginRequest==null){
-            return null;
+            return ResultUtils.error(ErrorCode.PARAMS_ERROR);
         }
         String userAccount = userLoginRequest.getUserAccount();
         String userPassword = userLoginRequest.getUserPassword();
         if (StringUtils.isAnyBlank(userAccount,userPassword)){
-            return null;
+            return ResultUtils.error(ErrorCode.PARAMS_ERROR);
         }
-        return userService.userLogin(userAccount,userPassword,request);
+        User user=userService.userLogin(userAccount,userPassword,request);
+        return ResultUtils.success(user);
     }
+
+
 
     /**
      * 用户搜索类
@@ -68,9 +76,9 @@ public class UserController {
      * @return 返回所有的用户或者模糊查询的用户
      */
     @GetMapping("/search")
-    public List<User> searchUsers(String username,HttpServletRequest request){
+    public BaseResponse<List<User>> searchUsers(String username,HttpServletRequest request){
         if (!isAdmin(request)){
-            return new ArrayList<>();
+            throw new BusinessException(ErrorCode.NO_AUTH, "缺少管理员权限");
         }
         QueryWrapper<User> queryWrapper=new QueryWrapper<>();
         if (StringUtils.isNotBlank(username)){
@@ -78,19 +86,21 @@ public class UserController {
         }
         List<User> userList=userService.list(queryWrapper);
         //进行用户脱敏
-        return userList.stream().map(user -> userService.safetyUser(user)).collect(Collectors.toList());
+        List<User> list=userList.stream().map(user -> userService.safetyUser(user)).collect(Collectors.toList());
+        return ResultUtils.success(list);
     }
 
     @PostMapping("/delete")
-    public boolean deleteUser(@RequestBody long id,HttpServletRequest request){
+    public BaseResponse<Boolean> deleteUser(@RequestBody long id,HttpServletRequest request){
         //判断是否是管理员
         if (!isAdmin(request)){
-            return false;
+            throw new BusinessException(ErrorCode.NO_AUTH);
         }
         if (id<=0){
-            return false;
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
         }
-        return userService.removeById(id);
+        boolean b = userService.removeById(id);
+        return ResultUtils.success(b);
     }
 
 
